8981335ee233c74c4a549738bf9af0354aad2c6e
Elee Lawleit <alibutt200235@gmail.com>
2023-05-20 13:15:47 +0500

    sessions using SharedPreferences

--- app/src/main/java/com/example/project/SignIn.java
+++ app/src/main/java/com/example/project/SignIn.java
@@ -4,22 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.se.omapi.Session;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.project.controllers.SignInController;
+import com.example.project.session.SessionManager;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class SignIn extends AppCompatActivity {
@@ -28,7 +25,28 @@ public class SignIn extends AppCompatActivity {
     Button btnSignIn;
 
     EditText etUsername;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //check for user session through the stored email
+        SessionManager sessionManager = new SessionManager(SignIn.this);
+        String email = sessionManager.getSession();
+        if(email != null)
+            moveToMainActivity();
+    }
+
     EditText etPassword;
+
+    //made a separate function bc it's being used twice
+    private void moveToMainActivity(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        //to ensure a fresh main activity (prevent any previous saved state from being loaded)
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish(); //because we don't want to return here after login
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +57,8 @@ public class SignIn extends AppCompatActivity {
         btnSignup = findViewById(R.id.btnSignUp);
         etUsername = findViewById(R.id.etUsernameSignin);
         etPassword = findViewById(R.id.etPasswordSignin);
+
+
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,14 +83,6 @@ public class SignIn extends AppCompatActivity {
 
                     //parse the response json
                     JsonObject jsonObject = JsonParser.parseString(responses.get().getResponseBody()).getAsJsonObject();
-                    Log.d("res", String.valueOf(jsonObject.get("res")));
-
-                    //to get nested json objects
-                    if(jsonObject.get("user") != null){
-                        JsonObject user = jsonObject.getAsJsonObject("user");
-                        Log.d("User name", user.get("name") + "");
-                        Log.d("User email", user.get("email") + "");
-                    }
 
                     if(responses.get().getResponseCode() == 404){
                         Snackbar.make(btnSignIn, "No user found", Snackbar.LENGTH_LONG).show();
@@ -79,9 +91,14 @@ public class SignIn extends AppCompatActivity {
                         Snackbar.make(btnSignIn, "Wrong password", Snackbar.LENGTH_LONG).show();
                     }
                     else if(responses.get().getResponseCode() == 200){
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                        finish(); //because we don't want to return here after login
+
+                        if(jsonObject.get("user") != null){
+                            JsonObject user = jsonObject.getAsJsonObject("user");
+                            //passing the current context
+                            SessionManager sessionManager = new SessionManager(SignIn.this);
+                            sessionManager.saveSession(user.get("email").toString());
+                            moveToMainActivity();
+                        }
                     }
                     else{
                         Snackbar.make(btnSignIn, "Server error. Please try again.", Snackbar.LENGTH_LONG).show();
